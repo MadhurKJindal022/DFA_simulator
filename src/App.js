46974fa12849b461de791f63fbc612d1948be0be
Madhur Kumar Jindal <madhurkjindal@gmail.com>
2025-08-14 16:21:55 +0530

    Add homepage and project files for GitHub Pages deployment

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./components/Layout";
 import Visualizer from "./Pages/Visualizer";
 import Gallery from "./Pages/Gallery";
 
 function App() {
   return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Navigate to="/Visualizer" />} />
-          <Route path="/Visualizer" element={<Visualizer />} />
-          <Route path="/Gallery" element={<Gallery />} />
-        </Routes>
-      </Layout>
-    </Router>
+    <Layout>
+      <Routes>
+        <Route index element={<Navigate to="Visualizer" replace />} />
+        <Route path="Visualizer" element={<Visualizer />} />
+        <Route path="Gallery" element={<Gallery />} />
+        <Route path="*" element={<Navigate to="Visualizer" replace />} />
+      </Routes>
+    </Layout>
   );
 }
 
